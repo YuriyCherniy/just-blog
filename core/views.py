e9cc890123ab5be3838ff736f36a52a9d5ce6873
Yuriy Cherniy <amidobox@gmail.com>
2021-07-06 21:58:38 +0300

    fixed IndexView

--- core/views.py
+++ core/views.py
@@ -1,12 +1,9 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView
 
 from posts.models import Post
 
 
-class IndexView(TemplateView):
+class IndexView(ListView):
+    model = Post
+    paginate_by = 2
     template_name = 'core/index.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['posts'] = Post.objects.all()
-        return context
