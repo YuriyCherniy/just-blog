a878ecfcd533410e91b4b14b9528b17ed7095325
Yuriy Cherniy <amidobox@gmail.com>
2021-07-13 11:22:35 +0300

    added tag field to post model

--- posts/models.py
+++ posts/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from tinymce.models import HTMLField
 from django.urls import reverse
 
+from tags.models import Tag
+
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
@@ -9,6 +11,7 @@ class Post(models.Model):
     text = HTMLField()
     created = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
+    tag = models.ManyToManyField(Tag, blank=True)
 
     def get_absolute_url(self):
         return reverse('post_detail', args=[self.slug])
