f14da0c943b59bce42732caca4f91fcd56bde45f
Yuriy Cherniy <amidobox@gmail.com>
2021-07-13 11:23:47 +0300

    added tags app to urls

--- just_blog/urls.py
+++ just_blog/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('tinymce/', include('tinymce.urls')),
     path('post/', include('posts.urls')),
+    path('tag/', include('tags.urls'))
 ]
---
+++ tags/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from tags.views import PostListFromTag
+
+
+urlpatterns = [
+    path('<str:slug>', PostListFromTag.as_view(), name='post_from_tag')
+]
