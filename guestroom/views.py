b9c7f27ab7b4bf4afbab8ae35ddce720bd7af78a
Yuriy Cherniy <amidobox@gmail.com>
2021-07-22 11:46:36 +0300

    added messages framework

--- guestroom/views.py
+++ guestroom/views.py
@@ -14,8 +14,9 @@ class GuestPostList(ListView):
     paginate_by = 5
 
 
-class GuestPostDetail(DetailView):
+class GuestPostDetail(LoginRequiredMixin, DetailView):
     model = GuestPost
+    raise_exception = True
 
 
 class GuestPostCreate(SuccessMessageMixin, CreateView):
@@ -25,21 +26,28 @@ class GuestPostCreate(SuccessMessageMixin, CreateView):
     success_message = 'Сообщение добавлено'
 
 
-class GuestPostUpdate(LoginRequiredMixin, UpdateView):
+class GuestPostUpdate(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
     model = GuestPost
     fields = ['anonymous_username', 'text']
     template_name = 'guestroom/guestpost_update_form.html'
     success_url = reverse_lazy('guest_room')
     raise_exception = True
+    success_message = 'Сообщение обновлено'
 
 
-class GuestPostDelete(LoginRequiredMixin, DeleteView):
+class GuestPostDelete(LoginRequiredMixin, SuccessMessageMixin, DeleteView):
     model = GuestPost
     success_url = reverse_lazy('guest_room')
     raise_exception = True
 
+    def post(self, request, *args, **kwargs):
+        messages.success(request, 'Сообщение удалено')
+        return super().post(request, *args, **kwargs)
 
-class GuestCommentCreate(CreateView):
+
+class GuestCommentCreate(LoginRequiredMixin, SuccessMessageMixin, CreateView):
+    raise_exception = True
+
     def post(self, request):
         form = GuestCommentForm(request.POST)
         if form.is_valid():
@@ -48,18 +56,21 @@ class GuestCommentCreate(CreateView):
                 text=form.cleaned_data['text'],
                 guest_post_id=form.cleaned_data['guest_post_pk']
             )
+            messages.success(request, 'Комментарий добавлен')
             return redirect(reverse('guest_room'))
 
+        messages.warning(request, 'Проверьте данные')
         return render(
             request, 'guestroom/guestcomment_form.html', {'form': form}
         )
 
 
-class GuestCommentUpdate(LoginRequiredMixin, UpdateView):
+class GuestCommentUpdate(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
     model = GuestComment
     fields = ['username', 'text']
     template_name = 'guestroom/guestcomment_update_form.html'
     success_url = reverse_lazy('guest_room')
+    success_message = 'Комментарий обнавлён'
     raise_exception = True
 
 
@@ -67,3 +78,7 @@ class GuestCommentDelete(LoginRequiredMixin, DeleteView):
     model = GuestComment
     success_url = reverse_lazy('guest_room')
     raise_exception = True
+
+    def post(self, request, *args, **kwargs):
+        messages.success(request, 'Комментарий удалён')
+        return super().post(request, *args, **kwargs)
