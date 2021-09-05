c4a76f729adcb3e7c50a25196b0455cacaed1bb0
Yuriy Cherniy <amidobox@gmail.com>
2021-09-05 23:35:17 +0300

    move static folder to core app

--- just_blog/settings.py
+++ just_blog/settings.py
@@ -189,7 +189,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 STATICFILES_DIRS = [
