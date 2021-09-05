62137fa8382404734134d5c78b0e93690a43f635
Yuriy Cherniy <amidobox@gmail.com>
2021-09-05 23:46:17 +0300

    minor changes

--- just_blog/settings.py
+++ just_blog/settings.py
@@ -192,9 +192,9 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
