858fa31cdc310fc43fe0b4798274ce806a99cc08
Yuriy Cherniy <amidobox@gmail.com>
2021-09-06 12:48:52 +0300

    minor changes

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,8 +5,8 @@
 # python manage.py collectstatic --noinput
 
 # Apply database migrations
-# echo "Apply database migrations"
-# python3 manage.py migrate
+echo "Apply database migrations"
+python3 manage.py migrate
 
 # Start server
 echo "Starting server"
