7fa6057970d7ba5fe81cef4e1c8140401beca358
Yuriy Cherniy <amidobox@gmail.com>
2021-09-10 22:28:03 +0300

    added random posts under text of post detail page

---
+++ posts/templatetags/get_random_posts_tag.py
@@ -0,0 +1,15 @@
+from django import template
+
+from posts.models import Post
+
+
+register = template.Library()
+
+
+@register.inclusion_tag('posts/random_posts.html')
+def show_posts(current_post_pk):
+    '''
+    Shows random posts under text of post detail page
+    '''
+    five_random_posts = Post.objects.order_by('?').exclude(pk=current_post_pk)[:3]
+    return {'five_random_posts': five_random_posts}
