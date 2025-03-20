403e449613fed2bc76a0364cec2728ee02191a9d
Yuriy <YuriyCherniyMail@yandex.ru>
2025-03-20 22:19:51 +0300

    .

--- app/just_blog/settings.py
+++ app/just_blog/settings.py
@@ -121,7 +121,7 @@ DATABASES = {
         'NAME': env('POSTGRES_DB'),
         'USER': env('POSTGRES_USER'),
         'PASSWORD': env('POSTGRES_USER_PASSWORD'),
-        'HOST': 'localhost',
+        'HOST': 'db',
         'PORT': 5432,
     }
 }
