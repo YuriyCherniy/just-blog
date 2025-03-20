c792bdc642fd68a13470d05cf43a83ceb9dec7b2
Yuriy <YuriyCherniyMail@yandex.ru>
2025-03-20 23:00:21 +0300

    .

--- app/just_blog/settings.py
+++ app/just_blog/settings.py
@@ -120,7 +120,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': env('POSTGRES_DB'),
         'USER': env('POSTGRES_USER'),
-        'PASSWORD': env('POSTGRES_PASSWORD'),
+        'PASSWORD': env('POSTGRES_USER_PASSWORD'),
         'HOST': 'db',
         'PORT': 5432,
     }
